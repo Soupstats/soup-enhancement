75081b881de83aa3a916d8fa95f05df71d11e34c
SM <s.marx@spa-gmbh.de>
2017-03-23 10:30:56 +0100

    added modules mobile-friendly and search-box, reorganised folders

---
+++ mobile-friendly/mobile-friendly.js
@@ -0,0 +1,7 @@
+function soupENH_mobileFriendly()
+{
+    var meta = document.createElement("meta");
+    meta.name = "viewport";
+    meta.content = "width=device-width, initial-scale=1";
+    document.getElementsByTagName("head")[0].appendChild(meta);
+}
